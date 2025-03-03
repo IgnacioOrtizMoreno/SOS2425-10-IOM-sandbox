931c7e253bc14b9db10553594a322ee1d2c5f509
root <root@IgnacioLaptop.>
2025-03-03 15:41:47 +0100

    adding changes

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log("Init script");
+const express = require("express");
+const cool = require("cool-ascii-faces")
+const app = express();
+const PORT= 16078;
+
+//Ruta estatica
+
+app.use("/",express.static("./static"))
+
+function handler (request, response){
+        response.send("Hello from the server");
+    };
+
+app.get("/",handler)    
+
+
+// app.get("/",(request, response)=>{
+//     response.send("Hello from the server");
+// });
+app.get("/cool",(request, response)=>{
+    response.send(cool());
+});
+
+app.listen(PORT, ()=>{
+    console.log(`Server running on ${PORT}`);
+});
+
+console.log(`Finish Set up`);
