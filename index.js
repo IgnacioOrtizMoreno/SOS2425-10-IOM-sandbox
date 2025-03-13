5bebffee90e6fd02045bc11d5ed5de1c350af10a
root <root@IgnacioLaptop.>
2025-03-13 14:37:53 +0100

    l05

--- index.js
+++ index.js
@@ -26,3 +26,33 @@ app.listen(PORT, ()=>{
 });
 
 console.log(`Finish Set up`);
+
+//L05
+app.use(express.json());
+const BASE_API = "/api/v1"
+let contacts = [
+    {
+        name: "peter",
+        phone: 123456
+    },
+    {
+        name: "pablo",
+        phone: 678595
+    }
+]
+
+app.get(BASE_API+"/contacts",(request,response)=>{
+    console.log("New GET to /contacts"); // Se ven en el servidor en este caso localhost
+    response.send(JSON.stringify(contacts,null,2));
+});
+
+app.post(BASE_API+"/contacts",(request,response)=>{
+    console.log("POST to /contacts"); // Se ven en el servidor en este caso localhost
+    let newContact = request.body;
+
+    contacts.push(newContact);
+
+    response.sendStatus(201);
+});
+
+
